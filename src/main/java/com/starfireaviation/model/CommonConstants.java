401007e2f0e73f6671a2515248a640ab5336b86f
Brian Michael <brian@cajunbug.net>
2022-10-19 06:16:50 -0400

    Moving CommonConstants

---
+++ src/main/java/com/starfireaviation/model/CommonConstants.java
@@ -0,0 +1,134 @@
+/*
+ *  Copyright (C) 2022 Starfire Aviation, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.starfireaviation.model;
+
+/**
+ * Common Application Constants.
+ */
+public class CommonConstants {
+
+    /**
+     * ID.
+     */
+    public static final String ID = "ID";
+
+    /**
+     * Organization Header Key.
+     */
+    public static final String ORGANIZATION_HEADER_KEY = "organization";
+
+    /**
+     * CorrelationID Header Key.
+     */
+    public static final String CORRELATION_ID_HEADER_KEY = "correlation-id";
+
+    /**
+     * ClientID Header Key.
+     */
+    public static final String CLIENT_ID_HEADER_KEY = "groundschool";
+
+    /**
+     * THREE.
+     */
+    public static final int THREE = 3;
+
+    /**
+     * FOUR.
+     */
+    public static final int FOUR = 4;
+
+    /**
+     * FIVE.
+     */
+    public static final int FIVE = 5;
+
+    /**
+     * SIX.
+     */
+    public static final int SIX = 6;
+
+    /**
+     * SEVEN.
+     */
+    public static final int SEVEN = 7;
+
+    /**
+     * EIGHT.
+     */
+    public static final int EIGHT = 8;
+
+    /**
+     * TEN.
+     */
+    public static final int TEN = 10;
+
+    /**
+     * SIXTEEN.
+     */
+    public static final int SIXTEEN = 16;
+
+    /**
+     * THIRTY_TWO.
+     */
+    public static final int THIRTY_TWO = 32;
+
+    /**
+     * ONE_HUNDRED.
+     */
+    public static final int ONE_HUNDRED = 100;
+
+    /**
+     * TWO_HUNDRED_FIFTY_FIVE.
+     */
+    public static final int TWO_HUNDRED_FIFTY_FIVE = 255;
+
+    /**
+     * FIVE_HUNDRED.
+     */
+    public static final int FIVE_HUNDRED = 500;
+
+    /**
+     * FOUR_HUNDRED_EIGHTY.
+     */
+    public static final int FOUR_HUNDRED_EIGHTY = 480;
+
+    /**
+     * SIX_HUNDRED_FORTY.
+     */
+    public static final int SIX_HUNDRED_FORTY = 640;
+
+    /**
+     * ONE_THOUSAND.
+     */
+    public static final int ONE_THOUSAND = 1000;
+
+    /**
+     * TWO_THOUSAND.
+     */
+    public static final int TWO_THOUSAND = 2000;
+
+    /**
+     * FOUR_THOUSAND.
+     */
+    public static final int FOUR_THOUSAND = 4000;
+
+    /**
+     * FIVE_HUNDRED_THOUSAND.
+     */
+    public static final int FIVE_HUNDRED_THOUSAND = 5000000;
+
+}
